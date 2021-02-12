793d3f9dde425eac4f64eb93d7c54b12b5a8c136
Volodumyr Kolomiets <kolomiecnew@gmail.com>
2021-02-12 22:47:46 +0200

    homework 8

--- homework/flask/Amazon_killer.py
+++ homework/flask/Amazon_killer.py
@@ -1,48 +1,139 @@
+import flask
 from flask import Flask, request
+
 from datetime import datetime
 
 amazon_killer = Flask(__name__)
 
-USERS_DATABASE = {}
+USER_DATABASE = dict()
 user_counter = 1
+CART_DATABASE = dict()
+cart_counter = 1
 
 
-class NoSuchUser(Exception):
+class UserNotFound(Exception):
     def __init__(self, user_id):
         self.user_id = user_id
 
 
-@amazon_killer.route('/users', methods=["POST"])
-def create_user():
+class CartNotFound(Exception):
+    def __init__(self, cart_id):
+        self.cart_id = cart_id
+
+
+@amazon_killer.route('/users', methods=['POST'])
+def create_add_user():
     global user_counter
     user = request.json
     user['user_id'] = user_counter
+    dt = datetime.now().isoformat()
     response = {
-        "registration_timestamp": datetime.now().isoformat(),
-        "user_id": user_counter
+        'registration_timestamp': dt,
+        'user_id': user_counter
     }
-    user["registration_timestamp"] = response['registration_timestamp']
-    USERS_DATABASE[user_counter] = user
+    user['registration_timestamp'] = dt
+    USER_DATABASE[user_counter] = user
 
     user_counter += 1
+    status_code = 201
 
-    return response, 201
-
-
-@amazon_killer.errorhandler(NoSuchUser)
-def no_such_user_handler(e):
-    return {"error": "no such user with id 1"}, 404
+    return response, status_code
 
 
-@amazon_killer.route('/users/<int:user_id>')
+@amazon_killer.route('/users/<int:user_id>', methods=['PUT'])
+def update_user(user_id):
+    if user_id not in USER_DATABASE:
+        raise UserNotFound(user_id)
+    update_data_user = request.json
+    for key, value in USER_DATABASE.items():
+        if key == user_id:
+            value["name"] = update_data_user["name"]
+            value["email"] = update_data_user["email"]
+        response = {
+            "status": "success"
+        }
+        return response, 
+
+
+@amazon_killer.errorhandler(UserNotFound)
+def user_not_found(error):
+    return {"error": f"no such user with id {error.user_id}"}, 404
+
+
+@amazon_killer.route('/users/<int:user_id>', methods=['GET'])
 def get_user(user_id):
-    try:
-        user = USERS_DATABASE[user_id]
-    except KeyError:
-        raise NoSuchUser(user_id)
-    else:
+    user = USER_DATABASE.get(user_id)
+    if user is not None:
         return user
+    raise UserNotFound(user_id)
 
 
-if __name__ == '__main__':
-    amazon_killer.run(debug=True)
+@amazon_killer.route('/users/<int:user_id>', methods=['DELETE'])
+def delete_user(user_id):
+    if user_id not in USER_DATABASE:
+        raise UserNotFound(user_id)
+    USER_DATABASE.pop(user_id)
+    response = {"status": "success"}
+    return response
+
+
+@amazon_killer.route('/add_carts', methods=['POST'])
+def add_user_cart():
+    global cart_counter
+    USER_DATABASE[1] = dict()
+    cart = request.json
+    user_id = cart.get("user_id")
+    if user_id not in USER_DATABASE:
+        raise UserNotFound(user_id)
+    dt = datetime.now().isoformat()
+    response = {"cart_id": cart_counter,
+                "creation_time": dt}
+    cart["creation_time"] = response["creation_time"]
+    CART_DATABASE[cart_counter] = cart
+    cart_counter += 1
+    status_code = 201
+
+    return response, status_code
+
+
+@amazon_killer.errorhandler(CartNotFound)
+def cart_not_found(error):
+    return {"error": f"no such cart with id {error.cart_id}"}, 404
+
+
+@amazon_killer.route('/read_cart/<int:cart_id>', methods=['GET'])
+def read_cart_user(cart_id):
+    cart = CART_DATABASE.get(cart_id)
+    if cart is None:
+        raise CartNotFound(cart_id)
+    return cart
+
+
+@amazon_killer.route('/update_cart/<int:cart_id>', methods=['PUT'])
+def update_cart(cart_id):
+    cart_data = CART_DATABASE.get(cart_id)
+
+    if cart_data is None:
+        raise CartNotFound(cart_id)
+
+    new_list_products = request.json.get("products")
+    for key, value in CART_DATABASE.items():
+        if key == "products":
+            value.append(new_list_products)
+
+    response = {"status": "success"}
+
+    return response
+
+
+@amazon_killer.route('/del_cart/<int:cart_id>', methods=['DELETE'])
+def dell_user_cart(cart_id):
+    cart = CART_DATABASE.get(cart_id)
+
+    if cart is None:
+        raise CartNotFound(cart_id)
+
+    CART_DATABASE.pop(cart_id)
+    response = {"status": "success"}
+
+    return response
